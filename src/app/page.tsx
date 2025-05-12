6696f10dbb755eab5eb3540560302a823ce8977c
Mohan-Balaji <bmohanbalaji1976@gmail.com>
2025-05-12 18:36:13 +0530

    Navbar and Footer Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Footer from "../../components/footer/Footer";
 
 export default function Home() {
   return (
     <div className="">
-      <h1>Blog Website</h1>
+       <Footer />
     </div>
   );
 }
